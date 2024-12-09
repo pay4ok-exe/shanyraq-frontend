07e19629df0ed39ddd6dc88db03d83be32e2956f
S <kgr45637@mail.ru>
2024-12-10 03:34:40 +0500

    little fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
-        domains: ['findroommate.s3.eu-north-1.amazonaws.com', 'krisha-photos.kcdn.online', 'alaps-photos-kr.kcdn.kz'],
+        domains: ['findroommate.s3.eu-north-1.amazonaws.com', 'krisha-photos.kcdn.online', 'alaps-photos-kr.kcdn.kz', 'alakcell-photos-kr.kcdn.kz'],
     },
 };
 
